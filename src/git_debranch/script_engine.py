858e4c4888a29fd630538db6f09e6f5f0234ea90
Jon Herron <jon.herron@yahoo.com>
2023-05-18 08:13:18 -0400

    Running git fetch -p

--- src/git_debranch/script_engine.py
+++ src/git_debranch/script_engine.py
@@ -1,11 +1,23 @@
+import json
+import subprocess
+
 from SpiffWorkflow.bpmn.PythonScriptEngine import PythonScriptEngine
 
 class CustomScriptEngine(PythonScriptEngine):
     def call_service(self, operation_name, operation_params, task_data):
         operations = {
+            "git/FetchAndPrune": git_fetch_and_prune,
             "subprocess/Run": subprocess_run,
         }
         return operations[operation_name](operation_params, task_data)
 
+def git_fetch_and_prune(_params, task_data):
+    run_args = ["git", "fetch", "-p"]
+    params = {"args": {"value": run_args, "type": "any"}}
+    return subprocess_run(params, task_data)
+    
 def subprocess_run(params, task_data):
+    args = params["args"]["value"]
+    result = subprocess.run(args, capture_output=True)
+    print(result)
     return "{}"
