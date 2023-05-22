2c58a31bff62174911105d3c96637a31ddd1e5ac
Jon Herron <jon.herron@yahoo.com>
2023-05-21 21:43:18 -0400

    v0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def contents_of(filename):
 
 setup(
     name = "git-debranch",
-    version = "0.1.0",
+    version = "0.1.1",
     description = "Remove git branches",
     long_description = contents_of("README.md"),
     long_description_content_type = "text/markdown",
