242331785671fad46d1eb8290ef21a0b223649e1
Jon Herron <jon.herron@yahoo.com>
2023-05-24 07:22:53 -0400

    All git operations are spawn process

--- src/git_debranch/script_engine.py
+++ src/git_debranch/script_engine.py
@@ -6,41 +6,10 @@ from SpiffWorkflow.bpmn.PythonScriptEngine import PythonScriptEngine
 class CustomScriptEngine(PythonScriptEngine):
     def call_service(self, operation_name, operation_params, task_data):
         operations = {
-            "git/DeleteBranches": git_delete_branches,
-            "git/ListAllMergedBranches": git_list_all_merged_branches,
-            "git/ListAllUnmergedBranches": git_list_all_unmerged_branches,
             "os/SpawnProcess": spawn_process,
         }
         return operations[operation_name](operation_params, task_data)
 
-def git_delete_branches(params, task_data):
-    run_args = ["git", "branch", "-D"]
-    branches_to_delete = task_data["branches_to_delete"]
-    run_args += branches_to_delete
-    params = {"args": {"value": run_args, "type": "any"}}
-    return subprocess_run(params, task_data)
-
-def git_list_all_merged_branches(params, task_data):
-    run_args = ["git", "branch", "--all", "--merged"]
-    params = {"args": {"value": run_args, "type": "any"}}
-    return subprocess_run(params, task_data)
-
-def git_list_all_unmerged_branches(params, task_data):
-    run_args = ["git", "branch", "--all", "--no-merged"]
-    params = {"args": {"value": run_args, "type": "any"}}
-    return subprocess_run(params, task_data)
-
-def subprocess_run(params, task_data):
-    args = params["args"]["value"]
-    subprocess_result = subprocess.run(args, capture_output=True)
-    result = {
-        "returncode": subprocess_result.returncode,
-        "stdout": subprocess_result.stdout.decode("utf-8"),
-        "stderr": subprocess_result.stderr.decode("utf-8"),
-    }
-    return json.dumps(result)
-
-# TODO: this will be used and above become BPMN call activities
 def spawn_process(params, task_data):
     args = params["args"]["value"]
     subprocess_result = subprocess.run(args, capture_output=True)
